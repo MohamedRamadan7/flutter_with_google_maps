591711726095c524f9b95cbc5c1ee7514e42dd7b
MohamedRamadan7 <mohamedramadan6420@Gmail.com>
2025-01-15 10:13:51 +0200

    LocationService and LocationPermission and Locationdata

--- lib/widgets/google_map_item.dart
+++ lib/widgets/google_map_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 import 'map_style.dart';
 
@@ -12,18 +13,20 @@ class GoogleMapItem extends StatefulWidget {
 
 class _GoogleMapItemState extends State<GoogleMapItem> {
   late CameraPosition initialCameraPosition;
-  late GoogleMapController googleMapController;
-
+  GoogleMapController? googleMapController;
+  late Location location;
   @override
   void initState() {
     initialCameraPosition = const CameraPosition(
         zoom: 12, target: LatLng(31.187084851056554, 29.928110526889437));
+    location = Location();
+    updateMyLocation();
     super.initState();
   }
 
   @override
   void dispose() {
-    googleMapController.dispose();
+    googleMapController!.dispose();
     super.dispose();
   }
 
@@ -44,7 +47,7 @@ class _GoogleMapItemState extends State<GoogleMapItem> {
           left: 16,
           child: ElevatedButton(
             onPressed: () {
-              googleMapController.animateCamera(CameraUpdate.newLatLng(
+              googleMapController!.animateCamera(CameraUpdate.newLatLng(
                   LatLng(31.158887525900187, 31.482723748533026)));
               setState(() {});
             },
@@ -53,5 +56,50 @@ class _GoogleMapItemState extends State<GoogleMapItem> {
         )
       ],
     );
+  }
+
+  Future<void> checkAndRequestLocationService() async {
+    bool isServiceEnabled = await location.serviceEnabled();
+    if (!isServiceEnabled) {
+      isServiceEnabled = await location.requestService();
+      if (!isServiceEnabled) {
+        //ToDo: show error bar
+      }
+    }
+    checkAndRequestLocationPermission();
+  }
+
+  Future<bool> checkAndRequestLocationPermission() async {
+    PermissionStatus permissionStatus = await location.hasPermission();
+    if (permissionStatus == PermissionStatus.deniedForever) {
+      return false;
+    }
+    if (permissionStatus == PermissionStatus.denied) {
+      permissionStatus = await location.requestPermission();
+      if (permissionStatus != PermissionStatus.granted) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  void getLocationdata() async {
+    location.onLocationChanged.listen((locationData) {
+      CameraPosition cameraPosition = CameraPosition(
+        target: LatLng(locationData.latitude!, locationData.longitude!),
+      );
+      googleMapController
+          ?.animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
+    });
+  }
+
+  void updateMyLocation() async {
+    await checkAndRequestLocationService();
+    bool hasPermission = await checkAndRequestLocationPermission();
+    if (hasPermission) {
+      getLocationdata();
+    } else {
+      //ToDo: show error bar No permission
+    }
   }
 }
